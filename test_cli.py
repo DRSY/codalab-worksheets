c13eb986782c1788981a8aa0c3481c26213a4a5d
Tony Lee <tonyh.lee@yahoo.com>
2020-05-11 23:31:34 -0700

    fix dependency path

--- test_cli.py
+++ test_cli.py
@@ -495,6 +495,24 @@ def test(ctx):
     check_contains('0x', get_info(uuid, 'data_hash'))
 
 
+@TestModule.register('dependencies')
+def test(ctx):
+    # TODO: -tony
+    # Test that content of dependency is mounted at top
+    dir1 = _run_command([cl, 'upload', test_path('dir1')])
+    uuid = _run_command([cl, 'run', ':%s' % dir1, 'cat f1'])
+    check_equals('first file', _run_command([cl, 'cat', uuid]))
+
+    # Check that referencing a file from the dependency key still works
+    uuid = _run_command([cl, 'run', ':%s' % dir1, 'cat {}/f1'.format(dir1)])
+    check_equals('first file', _run_command([cl, 'cat', uuid]))
+
+    # We should respect the content of the subsequent dependency with the same name
+    dir2 = _run_command([cl, 'upload', test_path('dir2')])
+    uuid = _run_command([cl, 'run', ':%s' % dir1, ':%s' % dir2, 'cat f1'])
+    check_equals('first file in dir2', _run_command([cl, 'cat', uuid]))
+
+
 @TestModule.register('upload1')
 def test(ctx):
     # Upload contents
